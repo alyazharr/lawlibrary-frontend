33fc26224265c96fc0eb10dfe049ad46e9b4e543
Denny Johannes Hasea <denny.johannes@ui.ac.id>
2023-04-19 18:45:30 +0700

    Initial commit

---
+++ src/components/Header.js
@@ -0,0 +1,42 @@
+import { useRef } from "react";
+import { FaBars, FaTimes } from "react-icons/fa";
+import "../Styles/Header.css";
+
+function Header() {
+    const navRef = useRef();
+
+    const showNavbar = () => {
+        navRef.current.classList.toggle(
+            "responsive_nav"
+        );
+    };
+
+    return (
+        <header>
+            <h3><a href="/">LawLibrary</a></h3>
+            <nav ref={navRef}>
+                <a href="/#">Profile</a>
+                <a href="/#">Book Review</a>
+                <a href="/#">Book Recommendation</a>
+                <button
+                    className="nav-btn nav-close-btn"
+                    onClick={showNavbar}>
+                    <FaTimes />
+                </button>
+            </nav>
+            <button
+                className="nav-btn"
+                onClick={showNavbar}>
+                <FaBars />
+            </button>
+            <div className="search-container">
+                <form action="/#">
+                    <input type="text" placeholder="Search.." name="search"/>
+                    <button type="submit">Submit</button>
+                </form>
+            </div>
+        </header>
+    );
+}
+
+export default Header;
