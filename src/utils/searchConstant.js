f393cbb0a856da635e4bd4105933e280b6d642b8
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2023-04-28 23:25:39 +0700

    integrasi search-service frontend

---
+++ src/utils/searchConstant.js
@@ -0,0 +1,3 @@
+export const API_SEARCH_TITLE = "http://localhost:8000/search/title"
+export const API_SEARCH_AUTHOR = "http://localhost:8000/search/author"
+export const API_SEARCH = "http://localhost:8000/search"
