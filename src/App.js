ecbd292b198efe56990bffcb51562d91c5398535
agnesaudya <70096521+agnesaudya@users.noreply.github.com>
2023-04-30 20:21:11 +0700

    restructure routing

--- src/App.js
+++ src/App.js
@@ -22,52 +22,54 @@ import Stock from "./components/Stock";
 function App() {
     return (
         <Routes>
-            <Route path="/" element={< Layout />} >
-                {/*public routes*/}
-                <Route
-                    path="/home" exact
-                    element={<Home />}
-                />
-                <Route
-                    path="auth/register"
-                    element={<RegistrationForm />}
-                />
-                <Route
-                    path="auth/login"
-                    element={<LoginForm />}
-                />
-                {/* belum ditambahin auth guys */}
-                <Route
-                            path="targetreminderform/:id"
-                            element={<TargetReminderForm />}
-                        />
-                <Route
-                            path="detailtargetreminder/:id/:idbuku"
-                            element={<DetailTargetReminder />}
-                        />
-                <Route
-                            path="detailBuku/:id"
-                            element={<DetailBuku />}
-                        />
-                <Route
-                            path="detailBuku/:id/stok"
-                            element={<Stock />}
-                        />
+            <Route element={<PersistLogin />}>
+                <Route path="/" element={< Layout />} >
+                    {/*public routes*/}
+                    <Route
+                        path="/home" exact
+                        element={<Home />}
+                    />
+                    <Route
+                        path="auth/register"
+                        element={<RegistrationForm />}
+                    />
+                    <Route
+                        path="auth/login"
+                        element={<LoginForm />}
+                    />
+                    {/* belum ditambahin auth guys */}
+                    <Route
+                        path="targetreminderform/:id"
+                        element={<TargetReminderForm />}
+                    />
+                    <Route
+                        path="detailtargetreminder/:id/:idbuku"
+                        element={<DetailTargetReminder />}
+                    />
+                    <Route
+                        path="detailBuku/:id"
+                        element={<DetailBuku />}
+                    />
+                    <Route
+                        path="detailBuku/:id/stok"
+                        element={<Stock />}
+                    />
                 </Route>
                 {/* belum ditambahin auth guys */}
                 <Route
                     path="books/search"
                     element={<Search />}
-                        />
+                />
 
                 {/*protected routes*/}
-                <Route element={<PersistLogin />}>
-                    <Route element={<RequireAuth />}>
-                        <Route
-                            path="profile"
-                            element={<Profile />}
-                        />
-                    </Route>
+
+                <Route element={<RequireAuth />}>
+                    <Route
+                        path="profile"
+                        element={<Profile />}
+                    />
+
+                </Route>
             </Route>
         </Routes>
     );
