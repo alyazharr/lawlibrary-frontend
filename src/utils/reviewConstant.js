a9345700b62bedc41b4c698b2786f133de3c1989
Alya Azhar Agharid <alya.azhar@ui.ac.id>
2023-05-02 07:10:30 +0700

    fix post logic

--- src/utils/reviewConstant.js
+++ src/utils/reviewConstant.js
@@ -5,7 +5,3 @@ export const API_REVIEW_CREATE = "http://34.72.52.78/reviews/add"
 export const API_REVIEW_UPDATE = "http://34.72.52.78/reviews/update"
 export const API_REVIEW_DELETE = "http://34.72.52.78/reviews/delete"
 export const API_REVIEW_RATING = "http://34.72.52.78/reviews/avg-rate"
-
-
-
-
